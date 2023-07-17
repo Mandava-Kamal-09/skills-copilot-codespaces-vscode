3cf5a1062fdf4fa97edc350e82ba823628c47ee0
Mandava-Kamal-09 <99558200+Mandava-Kamal-09@users.noreply.github.com>
2023-07-17 20:11:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember()) {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django']
+    var member = {
+        name: 'Rahul',
+        age: 20,
+        skills: skills
+    }
+    return member
+}
